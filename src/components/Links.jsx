b4c586ddabfa59d69d347b97486f8dde3daf7396
liuisaac <80558145+liuisaac@users.noreply.github.com>
2023-06-01 18:19:55 -0600

    Added Links tags/labels, minor scalability fixes
    modified:   src/components/Links.jsx
    modified:   src/gallery.css

--- src/components/Links.jsx
+++ src/components/Links.jsx
@@ -5,46 +5,46 @@ import "../gallery.css";
 const Links = () => {
   return (
     <section id="links" className={'flex flex-col relative z-5'}>
-      <div className="flex flex-row items-center justify-center my-5 text-white font-bold text-[20px]">
+      <div className="flex flex-row items-center justify-center my-5 text-white font-bold text-[40px]">
         QUICK LINKS
       </div>
-      <div className='gallery'>        
+      <div className='gallery lg:visible invisible'>        
         <div className="cards">
           <div className="card">
             <h3>
-              Photography
+              PHOTOGRAPHY
             </h3>
           </div>
 
           <div className="card">
             <h3>
-              Hello
+              AWARDS
             </h3>
           </div>
 
           <div className="card">
             <h3>
-              Hello
+              STREAM
             </h3>
           </div>
 
           <div className="card">
             <h3>
-              Hello
-            </h3>
-          </div>
-
-
-          <div className="card">
-            <h3>
-              Hello
+              APPAREL
             </h3>
           </div>
 
 
           <div className="card">
             <h3>
-              Hello
+              INFO
+            </h3>
+          </div>
+
+
+          <div className="card">
+            <h3>
+              VOLUNTEERS
             </h3>
           </div>
         </div>

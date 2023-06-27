5cf72e4d35f70ba055554b445edd70b8a16f2d6d
liuisaac <80558145+liuisaac@users.noreply.github.com>
2023-06-27 17:41:00 -0600

    Added Icons to sidebar
    new file:   src/assets/Vector/SideBarAssets/Completed.svg
    new file:   src/assets/Vector/SideBarAssets/Live.svg
    new file:   src/assets/Vector/SideBarAssets/OverUnder.svg
    new file:   src/assets/Vector/SideBarAssets/Prairies.svg
    new file:   src/assets/Vector/SideBarAssets/Rockies.svg
    new file:   src/assets/Vector/SideBarAssets/SpinUp.svg
    new file:   src/assets/Vector/SideBarAssets/Trailers.svg
    new file:   src/assets/Vector/SideBarAssets/Upcoming.svg
    modified:   src/assets/index.js
    modified:   src/components/Stream/Sidebar.jsx

--- src/components/Stream/Sidebar.jsx
+++ src/components/Stream/Sidebar.jsx
@@ -1,5 +1,5 @@
 import { React, useEffect, useState } from 'react'
-import { circle_arrow, circle_arrow_back } from '../../assets'
+import { circle_arrow, circle_arrow_back, completed, live, overunder, prairies_icon, rockies_icon, spinup, trailers, upcoming } from '../../assets'
 
 const Sidebar = () => {
   return (
@@ -15,7 +15,8 @@ const Sidebar = () => {
 
         <div className="w-full h-[10%] py-2 flex flex-row justify-start items-center 
                         border-t-[1px] border-r-[1px] border-gray-800 bg-[#121218] hover:bg-black
-                        transition duration-100 ease-in-out">
+                        transition duration-100 ease-in-out group">
+          <div className="w-12 ml-6 opacity-60 transition duration-200 ease-in-out group-hover:opacity-100"><img src={rockies_icon} /></div>
           <div className="flex flex-col justify-start items-start">
             <span className='ml-8 text-2xl font-normal'>THE ROCKIES</span>
             <span className='ml-8 -mt-2 tracking-wider'>DIVISION</span>
@@ -24,7 +25,8 @@ const Sidebar = () => {
 
         <div className="w-full h-[10%] py-2 flex flex-row justify-start items-center 
                         border-t-[1px] border-r-[1px] border-gray-800 bg-[#121218] hover:bg-black
-                        transition duration-100 ease-in-out">
+                        transition duration-100 ease-in-out group">
+          <div className="w-12 ml-6 opacity-60 transition duration-200 ease-in-out group-hover:opacity-100"><img src={prairies_icon} /></div>
           <div className="flex flex-col justify-start items-start">
             <span className='ml-8 text-2xl font-normal'>THE PRAIRIES</span>
             <span className='ml-8 -mt-2 tracking-wider'>DIVISION</span>
@@ -35,28 +37,31 @@ const Sidebar = () => {
 
         <div className="w-full h-[10%] py-2 flex flex-row justify-start items-center 
                         border-t-[1px] border-r-[1px] border-gray-800 bg-[#121218] hover:bg-black
-                        transition duration-100 ease-in-out">
+                        transition duration-100 ease-in-out group">
+          <div className="w-12 ml-[38px] opacity-60 transition duration-200 ease-in-out group-hover:opacity-100"><img src={upcoming} /></div>
           <div className="flex flex-col justify-start items-start">
-            <span className='ml-8 text-2xl font-normal'>UPCOMING</span>
-            <span className='ml-8 -mt-2 tracking-wider'>MATCHES</span>
+            <span className='ml-4 text-2xl font-normal'>UPCOMING</span>
+            <span className='ml-4 -mt-2 tracking-wider'>MATCHES</span>
           </div>
         </div>
 
         <div className="w-full h-[10%] py-2 flex flex-row justify-start items-center 
                         border-t-[1px] border-r-[1px] border-gray-800 bg-[#121218] hover:bg-black
-                        transition duration-100 ease-in-out">
+                        transition duration-100 ease-in-out group">
+          <div className="w-12 ml-[38px] opacity-60 transition duration-200 ease-in-out group-hover:opacity-100"><img src={live} /></div>
           <div className="flex flex-col justify-start items-start">
-            <span className='ml-8 text-2xl font-normal'>LIVE</span>
-            <span className='ml-8 -mt-2 tracking-wider'>MATCHES</span>
+            <span className='ml-4 text-2xl font-normal'>LIVE</span>
+            <span className='ml-4 -mt-2 tracking-wider'>MATCHES</span>
           </div>
         </div>
 
         <div className="w-full h-[10%] py-2 flex flex-row justify-start items-center 
                         border-t-[1px] border-r-[1px] border-gray-800 bg-[#121218] hover:bg-black
-                        transition duration-100 ease-in-out">
+                        transition duration-100 ease-in-out group">
+          <div className="w-12 ml-[38px] opacity-60 transition duration-200 ease-in-out group-hover:opacity-100"><img src={completed} /></div>
           <div className="flex flex-col justify-start items-start">
-            <span className='ml-8 text-2xl font-normal'>COMPLETED</span>
-            <span className='ml-8 -mt-2 tracking-wider'>MATCHES</span>
+            <span className='ml-4 text-2xl font-normal'>COMPLETED</span>
+            <span className='ml-4 -mt-2 tracking-wider'>MATCHES</span>
           </div>
         </div>
 
@@ -64,7 +69,8 @@ const Sidebar = () => {
 
         <div className="w-full h-[10%] py-2 flex flex-row justify-start items-center 
                         border-t-[1px] border-r-[1px] border-gray-800 bg-[#121218] hover:bg-black
-                        transition duration-100 ease-in-out">
+                        transition duration-100 ease-in-out group">
+          <div className="w-12 ml-6 opacity-60 transition duration-200 ease-in-out group-hover:opacity-100"><img src={trailers} /></div>
           <div className="flex flex-col justify-start items-start">
             <span className='ml-8 text-2xl font-normal'>TRAILERS</span>
             <span className='ml-8 -mt-2 tracking-wider'>REVEALS</span>
@@ -74,7 +80,8 @@ const Sidebar = () => {
         <div className="w-full h-[4%] py-2 flex flex-row justify-start items-end border-t-[1px] border-r-[1px] border-gray-800 font-semibold text-sm bg-black"><span className='ml-8'>YEAR</span></div>
         <div className="w-full h-[10%] py-2 flex flex-row justify-start items-center 
                         border-t-[1px] border-r-[1px] border-gray-800 bg-[#121218] hover:bg-black
-                        transition duration-100 ease-in-out">
+                        transition duration-100 ease-in-out group">
+          <div className="w-12 ml-6 opacity-60 transition duration-200 ease-in-out group-hover:opacity-100"><img src={overunder} /></div>
           <div className="flex flex-col justify-start items-start">
             <span className='ml-8 text-2xl font-normal'>2024</span>
             <span className='ml-8 -mt-2 tracking-wider'>VEX OVER UNDER</span>
@@ -82,7 +89,8 @@ const Sidebar = () => {
         </div>
         <div className="w-full h-[10%] py-2 flex flex-row justify-start items-center 
                         border-t-[1px] border-r-[1px] border-gray-800 bg-[#121218] hover:bg-black
-                        transition duration-100 ease-in-out">
+                        transition duration-100 ease-in-out group">
+          <div className="w-12 ml-6 opacity-60 transition duration-200 ease-in-out group-hover:opacity-100"><img src={spinup} /></div>
           <div className="flex flex-col justify-start items-start">
             <span className='ml-8 text-2xl font-normal'>2023</span>
             <span className='ml-8 -mt-2 tracking-wider'>VEX SPIN UP</span>
@@ -93,7 +101,8 @@ const Sidebar = () => {
 
         <div className="w-full h-[10%] py-2 flex flex-row justify-start items-center 
                         border-t-[1px] border-r-[1px] border-gray-800 bg-[#121218] hover:bg-black
-                        transition duration-100 ease-in-out">
+                        transition duration-100 ease-in-out group">
+          <div className="w-12 ml-6 opacity-60 transition duration-200 ease-in-out group-hover:opacity-100"><img src={rockies_icon} /></div>
           <div className="flex flex-col justify-start items-start">
             <span className='ml-8 text-2xl font-normal'>210Y</span>
             <span className='ml-8 -mt-2 tracking-wider'>#SHOUTOUT</span>
@@ -101,7 +110,8 @@ const Sidebar = () => {
         </div>
         <div className="w-full h-[10%] py-2 flex flex-row justify-start items-center 
                         border-t-[1px] border-r-[1px] border-gray-800 bg-[#121218] hover:bg-black
-                        transition duration-100 ease-in-out">
+                        transition duration-100 ease-in-out group">
+          <div className="w-12 ml-6 opacity-60 transition duration-200 ease-in-out group-hover:opacity-100"><img src={rockies_icon} /></div>
           <div className="flex flex-col justify-start items-start">
             <span className='ml-8 text-2xl font-normal'>3388H</span>
             <span className='ml-8 -mt-2 tracking-wider'>ANGEL</span>
@@ -109,7 +119,8 @@ const Sidebar = () => {
         </div>
         <div className="w-full h-[10%] py-2 flex flex-row justify-start items-center 
                         border-t-[1px] border-r-[1px] border-gray-800 bg-[#121218] hover:bg-black
-                        transition duration-100 ease-in-out">
+                        transition duration-100 ease-in-out group">
+          <div className="w-12 ml-6 opacity-60 transition duration-200 ease-in-out group-hover:opacity-100"><img src={rockies_icon} /></div>
           <div className="flex flex-col justify-start items-start">
             <span className='ml-8 text-2xl font-normal'>4082C</span>
             <span className='ml-8 -mt-2 tracking-wider'>FREEDOM SADIATORS</span>

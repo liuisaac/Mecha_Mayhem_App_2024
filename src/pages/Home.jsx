ca037454976f40387a0d909492e8d05aaeac79a8
liuisaac <80558145+liuisaac@users.noreply.github.com>
2023-07-02 20:19:05 -0600

    n

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,6 +3,10 @@ import { Hero, Links, Sponsors, Register, About_Mecha } from "../components/Home
 const Home = () => {
     return (
         <div className="bg-primary w-full overflow-hidden">
+            <div className='sm:hidden flex flex-col items-center justify-center text-center text-white h-screen w-full bg-primary pt-16'>
+                <h1 className='sm:text-6xl text-8xl italic font-bold'>WHOOPS.</h1>
+                <p className='w-3/4 text-3xl mt-[15vh]'>The mobile site for Mecha Mayhem is still under development. Until then, please use the desktop site for the best experience</p>
+            </div>
             <div className={'bg-gray-gradient ${styles.flexStart}'}>
                 <div className={'$(styles.boxWidth}'}>
                     <Hero />
